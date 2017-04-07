63b38c05dc505bb9a250d3e07aaeadaf67a51f6e
Adam Shakhabov <theadamo86@outlook.de>
2017-04-07 19:55:01 +0300

    work with IRepositoryV2<T>, IUnitOfWork

---
+++ monit95App.Infrastructure.Data/Repository.cs
@@ -0,0 +1,41 @@
+﻿using Monit95App.Domain.Core;
+using Monit95App.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monit95App.Infrastructure.Data
+{
+    public class Repository<T> : IRepositoryV2<T> where T : class
+    {
+        protected cokoContext _context;
+
+        public Repository(IUnitOfWork unitOfWork)
+        {
+            _context = unitOfWork.DbContext;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            IQueryable<T> query = _context.Set<T>();
+            return query;
+        }
+
+        public void Insert(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Attach(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+    }
+}
