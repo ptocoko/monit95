37edb8eab778128e550f8d8c26e39768fe5655fc
Adam Shakhabov <theadamo86@outlook.de>
2017-04-09 18:36:04 +0300

    added MarksCollectroController for Nsur project

--- Monit95App.Web/Scripts/controllers/planctrl.js
+++ Monit95App.Web/Scripts/controllers/planctrl.js
@@ -1,3 +1,11 @@
-﻿nsurApp.controller('planCtrl', function ($scope) {
+﻿nsurApp.controller('planCtrl', function ($scope) {    
+    $scope.uploadFile = function (f) {
+        console.log(f.files[0].name);
+        var formData = new FormData();
+        formData.append("uploadedFile", f.files[0]);
 
+        var obXhr = new XMLHttpRequest();
+        obXhr.open("POST", "/api/MarksCollector/UploadFile");
+        obXhr.send(formData);
+    }
 });
