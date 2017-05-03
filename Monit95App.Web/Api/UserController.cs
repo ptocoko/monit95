f53736afbbc2a587d96dcd8ccfecb43a629d2634
adamo86 <theadamo86@gmail.com>
2017-05-03 12:01:07 +0300

    added userservice

---
+++ Monit95App.Web/Api/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Monit95App.Api
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        //Get current user' name
+        public string GetName()
+        {
+            return User.Identity.Name;
+        }
+    }
+}
--- Monit95App.Web/app/particips/particips.component.ts
+++ Monit95App.Web/app/particips/particips.component.ts
@@ -1,8 +1,20 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+
+import { UserService } from './app/user.service';
 
 @Component({
     selector: 'particips',
     templateUrl: './app/particips/particips.html'
 })
 
-export class ParticipsComponent { }
+export class ParticipsComponent implements OnInit{
+    userName: string;
+
+    constructor(private userService: UserService) { };
+
+    ngOnInit() {
+        this.userService.GetName().subscribe(
+            response => this.userName = response
+        );
+    }
+}
