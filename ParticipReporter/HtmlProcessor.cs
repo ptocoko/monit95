940b7d18c07c0d0bed9e38f50cb3a727c9908443
Adam Shakhabov <theadamo86@outlook.de>
2017-05-14 02:48:56 +0300

    thread

--- ParticipReporter/HtmlProcessor.cs
+++ ParticipReporter/HtmlProcessor.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ParticipReporter
@@ -30,7 +31,8 @@ namespace ParticipReporter
             var htmlFiles = Directory.GetFiles(_htmlFolder);
             foreach (var item in htmlFiles)
             {
-                ConvertHtmlToPdf(item);             
+                var myThread = new Thread(ConvertHtmlToPdf);     
+                myThread.Start(item);                
             }          
         }
 
