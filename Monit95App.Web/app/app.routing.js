f2442b79d543abab0e5c4e7fd40b97775ee0dbba
adamo86 <theadamo86@gmail.com>
2017-05-16 10:55:54 +0300

    c

--- Monit95App.Web/app/app.routing.js
+++ Monit95App.Web/app/app.routing.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var router_1 = require("@angular/router");
 var particips_component_1 = require("./particips/particips.component");
 //import { ParticipListComponent } from './particips/particip-list.component';
