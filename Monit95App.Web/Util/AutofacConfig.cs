fad01c67793cc9081240944f34c627b9cc088b66
adamo86 <theadamo86@gmail.com>
2017-05-18 18:16:44 +0300

    c

--- Monit95App.Web/Util/AutofacConfig.cs
+++ Monit95App.Web/Util/AutofacConfig.cs
@@ -20,23 +20,7 @@ namespace Monit95App.Util
     public class AutofacConfig
     {
         public static void ConfigureContainer()
-        {
-            //// получаем экземпляр контейнера
-            //var builder = new ContainerBuilder();
-
-            //// регистрируем контроллер в текущей сборке
-            //builder.RegisterControllers(typeof(MvcApplication).Assembly);
-
-            //// регистрируем споставление типов
-            //builder.RegisterType<PParticipCodeCreator>().As<IPParticipCodeCreator>()
-            //    .WithParameter("db", new cokoContext());
-
-            //// создаем новый контейнер с теми зависимостями, которые определены выше
-            //var container = builder.Build();
-
-            //// установка сопоставителя зависимостей
-            //DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
-
+        {      
             var context = new cokoContext();
             context.Database.Initialize(false);
 
