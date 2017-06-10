dd7a8b748a4473a981e72ae2d422121c8db1d2b8
Adam Shakhabov <theadamo86@outlook.de>
2017-06-10 17:28:35 +0300

    work

--- Monit95App.Web/app/app.component.js
+++ Monit95App.Web/app/app.component.js
@@ -5,18 +5,31 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var user_service_1 = require("./user.service");
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(userService) {
+        this.userService = userService;
     }
+    AppComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.userService.getName().subscribe(function (response) {
+            var result = response.json();
+            _this.userHasAreaRole = result.IsAreaRole;
+        });
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'app-root',
         templateUrl: './app/app.component.html'
-    })
+    }),
+    __metadata("design:paramtypes", [user_service_1.UserService])
 ], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
--- Monit95App.Web/app/app.component.ts
+++ Monit95App.Web/app/app.component.ts
@@ -1,7 +1,21 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+    userHasAreaRole: boolean;
+
+    constructor(private userService: UserService) { }
+
+    ngOnInit() {
+        this.userService.getName().subscribe(
+            response => {
+                let result = response.json();           
+                this.userHasAreaRole = result.IsAreaRole;              
+            }
+        );
+    }
+}
