76567e5c7f655dbfc10f99e504bcc496a6c349c1
Adam Shakhabov <theadamo86@outlook.de>
2017-07-01 23:49:33 +0300

    c

---
+++ Tests/Monit95App.Infrastructure.Business.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Monit95App.Infrastructure.Business.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Monit95App.Infrastructure.Business.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5bebc720-a088-469e-a33b-a5da1c89974a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/Monit95App.Infrastructure.Business.Tests/RsurParticipReportServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Monit95App.Infrastructure.Business.Tests
+{
+    [TestClass]
+    public class RsurParticipReportServiceTest
+    {
+        [TestMethod]
+        public void GetTestsResultsGroupByParticipCodeTest()
+        {
+            //Arange
+            var unitOfWork = new Data.UnitOfWorkV2(new Domain.Core.cokoContext());
+            var repository = new Data.GenericRepository<Domain.Core.TestResult>(unitOfWork);
+            var service = new RsurParticipReportService(repository);
+
+            //Act
+            var participGroupResults = service.GetTestResultsGroupByParticipCode("873D064B-8039-4255-8FC5-C0CE7F711B59", new DateTime(2017, 5, 17));
+
+            //Assert
+            Assert.IsNotNull(participGroupResults);
+
+            //  Assert.Fail();
+        }
+    }
+}
