f542339c63b26c1240322f100500c629a9dd67e0
Adam Shakhabov <theadamo86@outlook.de>
2017-07-16 02:21:32 +0300

    finished unittest UserServiceTests.GetModelTest

--- Monit95App.Web.Tests/UserServiceTests.cs
+++ Monit95App.Web.Tests/UserServiceTests.cs
@@ -4,6 +4,9 @@ using System.Data.Entity;
 using Monit95App.Models;
 using Moq;
 using Monit95App.Web.Services;
+using System.Collections.Generic;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Linq;
 
 namespace Monit95App.Web.Tests
 {
@@ -13,16 +16,30 @@ namespace Monit95App.Web.Tests
         [TestMethod]
         public void GetModelTest()
         {
-            //Arrange
-            var mockSet = new Mock<IDbSet<ApplicationUser>>();
-            var mockContext = new Mock<ApplicationDbContext>();
-            mockContext.Setup(m => m.Users).Returns(mockSet.Object);
+            //Arrange         
+            var mockRoles = new List<IdentityUserRole>
+            {
+                new IdentityUserRole
+                {
+                    Role = new IdentityRole { Name = "school" }
+                }
+            };
 
+            var mockContext = new Mock<ApplicationDbContext>();
+            var mockSet = new Mock<IDbSet<ApplicationUser>>();            
+            var mockAppUser = new Mock<ApplicationUser>();
+
+            mockAppUser.SetupGet(x => x.UserName).Returns("0005");
+            mockAppUser.SetupGet(x => x.Roles).Returns(mockRoles);
+            mockContext.Setup(x => x.Users).Returns(mockSet.Object);
+            mockSet.Setup(x => x.Find(It.IsAny<string>())).Returns(mockAppUser.Object);
+            
             //Act
             var userService = new UserService(mockContext.Object);
+            var result = userService.GetModel("12345");
 
             //Assert
-            Assert.Fail();
+            Assert.IsTrue(result.UserRoleNames.First().Equals("school"));            
         }
     }
 }
