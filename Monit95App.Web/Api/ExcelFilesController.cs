50daacbbe409ef241b11b6cab4c27225b3def22d
Adam Shakhabov <theadamo86@outlook.de>
2017-08-21 08:15:36 +0300

    Add

---
+++ Monit95App.Web/Api/ExcelFilesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace Monit95App.Api
+{
+    [Authorize]
+    [RoutePrefix("api/ExcelFiles")]
+    public class ExcelFilesController : ApiController
+    {
+        [HttpPost]
+        [Route("")]
+        public IHttpActionResult Upload()
+        {            
+            var httpRequest = HttpContext.Current.Request;
+            if (httpRequest.Files.Count != 1)
+            {
+                return BadRequest();
+            }
+            var stream = httpRequest.Files[0].InputStream;
+
+            return Ok();
+        }
+    }
+}
