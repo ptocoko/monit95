e51230cd395004cb89c527d395b1d7f85ebc1aa5
Adam Shakhabov <theadamo86@outlook.de>
2017-09-11 12:42:25 +0300

    Change

--- Monit95App.Web/app/rsur/rsurparticip-add-form/rsurparticip-add-form.component.ts
+++ Monit95App.Web/app/rsur/rsurparticip-add-form/rsurparticip-add-form.component.ts
@@ -1,31 +1,105 @@
 ﻿import { Component, OnInit } from '@angular/core';
-import { DialogRef } from 'angular2-modal';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
-import { RsurParticip } from '../rsurparticip'
+import { RsurParticip } from '../rsurparticip';
+
 import { RsurParticipService } from '../rsurparticip.service';
+import { BasicValidators } from '../../shared/basic-validators';
 
 @Component({
-    selector: 'add-modal-form',
-    templateUrl: './app/rsur/add-modal-form/add-modal-form.html?v=${new Date().getTime()}'
+    selector: 'rsur-add-form',
+    templateUrl: './rsurparticip-add-form.component.html',
+    styleUrls: ['./rsurparticip-add-form.component.css']
 })
-export class AddModalFormComponent implements OnInit {
+export class RsurAddFormComponent implements OnInit {
+
+    form: FormGroup;
+    title: string;
     particip: RsurParticip;
 
     constructor(
-        private readonly dialog: DialogRef<RsurParticip>,
-        private readonly participService: RsurParticipService) {
+        formBuilder: FormBuilder,
+        private router: Router,
+        private route: ActivatedRoute,
+        private rsurParticipService: RsurParticipService
+    ) {
+        this.form = formBuilder.group({
+            surname: ['', [
+                Validators.required,
+                Validators.minLength(3)
+            ]],
+            name: ['', [
+                Validators.required,
+                Validators.minLength(3)
+            ]],
+            secondName: ['', [                
+                Validators.minLength(3)
+            ]],
+            categoryName: ['', [
+                Validators.required,
+                Validators.minLength(3)
+            ]],
+            experience: ['', [
+                Validators.required                
+            ]],
+            email: ['', [
+                Validators.required,
+                BasicValidators.email
+                //Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+            ]],
+            phone: ['', [
+                Validators.required,
+                Validators.pattern('[0-9]')
+            ]],
+            subjectName: ['', [
+                Validators.required                
+            ]],
+            birthday: ['', [
+                Validators.required
+            ]],
+            classNumbers: ['', [
+                Validators.required
+            ]],
+            address: formBuilder.group({
+                street: ['', Validators.minLength(3)],
+                suite: [],
+                city: ['', Validators.maxLength(30)],
+                zipcode: ['', Validators.pattern('^([0-9]){5}([-])([0-9]){4}$')]
+            })
+        });
     }
 
     ngOnInit() {
-        this.particip = this.dialog.context;
+        var id = this.route.params.subscribe(params => {
+            var id = params['id'];
+
+            this.title = id ? 'Edit User' : 'New User';
+
+            if (!id)
+                return;
+
+            this.usersService.getUser(id)
+                .subscribe(
+                user => this.user = user,
+                response => {
+                    if (response.status == 404) {
+                        this.router.navigate(['NotFound']);
+                    }
+                });
+        });
     }
 
-    onSubmit() {
-        console.log(this.particip);
-        this.dialog.close(this.particip);
-    }
+    save() {
+        var result,
+            userValue = this.form.value;
 
-    cancel() {
-        this.dialog.dismiss();
+        if (userValue.id) {
+            result = this.usersService.updateUser(userValue);
+        } else {
+            result = this.usersService.addUser(userValue);
+        }
+
+        result.subscribe(data => this.router.navigate(['users']));
     }
 }
