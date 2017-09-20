f0542d952273acf743844b13e5cff06ed9b19fb4
Хусайн Эсамбаев <husainesambaev@gmail.com>
2017-09-20 14:00:48 +0300

    fixed versions of templates

--- Monit95App.Web/app/class-particips/class-particips-plan.component.ts
+++ Monit95App.Web/app/class-particips/class-particips-plan.component.ts
@@ -1,7 +1,7 @@
 ﻿import { Component } from '@angular/core';
 
 @Component({
-	templateUrl: './app/class-particips/class-particips-plan.component.html?v=${new Date().getTime()}'
+	templateUrl: `./app/class-particips/class-particips-plan.component.html?v=${new Date().getTime()}`
 })
 export class ClassParticipsPlanComponent {
 
