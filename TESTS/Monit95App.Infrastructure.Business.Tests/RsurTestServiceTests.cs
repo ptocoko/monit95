b142cd75f0e6edd6d08a11e13908f9a77fd8ab9e
Adam Shakhabov <theadamo86@outlook.de>
2017-10-07 01:33:48 +0300

    Change

--- TESTS/Monit95App.Infrastructure.Business.Tests/RsurTestServiceTests.cs
+++ TESTS/Monit95App.Infrastructure.Business.Tests/RsurTestServiceTests.cs
@@ -3,13 +3,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Monit95App.Services.Tests
 {
+    using System.Linq;
+
+    using Monit95App.Infrastructure.Data;
+
     [TestClass]
     public class RsurTestServiceTests
     {
         [TestMethod]
         public void TestMethod1()
         {
-
+          
         }
     }
 }
