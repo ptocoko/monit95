e6487fa26fb425975832661e89c0185f092a739c
Хусайн Эсамбаев <husainesambaev@gmail.com>
2017-10-09 17:57:48 +0300

    after merge

--- Services/Monit95App.Services/RsurMarksService.cs
+++ Services/Monit95App.Services/RsurMarksService.cs
@@ -30,7 +30,7 @@ namespace Monit95App.Services
             var res = from particips in _participRepository.GetAll()
                       join participTest in _participTestRepository.GetAll() on particips.Code equals participTest.RsurParticipCode
                       where particips.School.AreaCode == areaCode && participTest.RsurTestId == rsurTestId
-                      join a in _resultRepository.GetAll() on participTest.Id equals a.ParticipTestId
+                      join a in _resultRepository.GetAll() on participTest.Id equals a.RsurParticipTestId
                       into b
                       from result in b.DefaultIfEmpty()
                       select new RsurParticipMarksListDto
@@ -39,7 +39,7 @@ namespace Monit95App.Services
                           Surname = particips.Surname,
                           Name = particips.Name,
                           SecondName = particips.SecondName,
-                          Marks = result == null ? null : result.Marks
+                          Marks = result == null ? null : result.RsurQuestionValues
                       };
 
             return res;
@@ -50,7 +50,7 @@ namespace Monit95App.Services
             var res = from participTest in _participTestRepository.GetAll()
                       where participTest.Id == participTestId
                       join particips in _participRepository.GetAll() on participTest.RsurParticipCode equals particips.Code
-                      join a in _resultRepository.GetAll() on participTest.Id equals a.ParticipTestId
+                      join a in _resultRepository.GetAll() on participTest.Id equals a.RsurParticipTestId
                       into b
                       from result in b.DefaultIfEmpty()
                       select new RsurGetMarksDto
@@ -59,7 +59,7 @@ namespace Monit95App.Services
                           Fio = particips.Surname + " " + particips.Name + " " + particips.SecondName,
                           TestNumberCodeWithName = participTest.RsurTest.Test.NumberCode + " - " + participTest.RsurTest.Test.Name.Trim(),
                           //TODO: realize MarksNames
-                          Marks = result == null ? null : result.Marks
+                          Marks = result == null ? null : result.RsurQuestionValues
                       };
 
             return res.SingleOrDefault();
@@ -72,14 +72,14 @@ namespace Monit95App.Services
             {
                 var rsurTestResult = new RsurTestResult
                 {
-                    ParticipTestId = participTestId,
-                    Marks = marks
+                    RsurParticipTestId = participTestId,
+                    RsurQuestionValues = marks
                 };
                 _resultRepository.Insert(rsurTestResult);
             }
             else
             {
-                rsurResult.Marks = marks;
+                rsurResult.RsurQuestionValues = marks;
                 _resultRepository.Update(rsurResult);
             }
         }
