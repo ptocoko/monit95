f2213acfcaa47dda7b9bc918bcc089d9176cece6
Adam Shakhabov <theadamo86@outlook.de>
2017-10-13 18:01:12 +0300

    Change

--- Monit95App.Web/Api/FileRepositoryController.cs
+++ Monit95App.Web/Api/FileRepositoryController.cs
@@ -4,28 +4,33 @@ using System.Web.Http;
 
 namespace Monit95App.Api
 {
+    using Monit95App.Services.Interfaces;
     using System.IO;
     using System.Net.Http.Headers;
 
     [Authorize]
-    [RoutePrefix("api/FileRepositories")]
-    public class FileRepositoriesController : ApiController
+    [RoutePrefix("api/FileRepository")]
+    public class FileRepositoryController : ApiController
     {
-        #region Fields
+        #region Dependencies
 
-        private const string RootPath = @"c:\FileRepositories";
+        private readonly IFileRepositoryManager fileRepositoryManager;
 
         #endregion
 
+        public FileRepositoryController(IFileRepositoryManager fileRepositoryManager)
+        {
+            this.fileRepositoryManager = fileRepositoryManager;
+        }
+
         #region APIs
-        
+
         [HttpGet]
         [Authorize(Roles = "area")]
         public HttpResponseMessage Get(string filePath) 
-        {
-            var fullFilePath = Path.Combine(RootPath, filePath); // check exist file
-            
-            var stream = new FileStream(fullFilePath, FileMode.Open, FileAccess.Read);
+        {            
+            var stream = fileRepositoryManager.GetFileStream(filePath, User.Identity.Name);
+
             var response = new HttpResponseMessage(HttpStatusCode.OK)
             {
                 Content = new StreamContent(stream)                
