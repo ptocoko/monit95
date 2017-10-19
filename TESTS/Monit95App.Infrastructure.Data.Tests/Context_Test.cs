6cc9e9af836b8fb994f73803ea2074e2a0c016ed
Хусайн Эсамбаев <husainesambaev@gmail.com>
2017-10-19 09:24:40 +0300

    created tests class for context

---
+++ TESTS/Monit95App.Infrastructure.Data.Tests/Context_Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Monit95App.Infrastructure.Data.Tests
+{
+    [TestClass]
+    public class Context_Test
+    {
+        private CokoContext _context;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _context = new CokoContext();
+        }
+
+        [TestMethod]
+        public void QuestionsEntity_Test()
+        {
+            var expected = "Fake School Name";
+
+            var actual = _context.Questions.Find(1).School.Name;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
