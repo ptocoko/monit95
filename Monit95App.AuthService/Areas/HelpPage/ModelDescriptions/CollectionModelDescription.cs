774d4a58a653586206d72a760cb52b00644fdf5e
Adam Shakhabov <theadamo86@outlook.de>
2017-11-08 12:09:49 +0300

    Add project

---
+++ Monit95App.AuthService/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Monit95App.AuthService.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
