32e681fb6f5bb35d1a248b617c3e82941641e846
Хусайн Эсамбаев <husainesambaev@gmail.com>
2017-11-10 14:12:26 +0300

    allowed 15MB photos

--- Monit95App.Web/app/components/rsur/seminar-reports/seminar-report/create-form/create-form.component.ts
+++ Monit95App.Web/app/components/rsur/seminar-reports/seminar-report/create-form/create-form.component.ts
@@ -42,7 +42,7 @@ export class CreateReportFormComponent implements OnInit {
 			return false;
 		}
 		for (var i = 0; i < files.length; i++) {
-			if (files[i].size / 1024 / 1024 > 10) {
+			if (files[i].size / 1024 / 1024 > 15) {
 				alert('Размер файла ' + files[i].name + ' превышает максимально разрешенный. \nМаксимально разрешенный размер файла — 10 МБ')
 				return false;
 			}
