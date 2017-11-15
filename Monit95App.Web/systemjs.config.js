453edcc16a72cbdb5d277839e80ca06dea3e3c89
Adam Shakhabov <theadamo86@outlook.de>
2017-11-15 17:55:45 +0300

    Change

--- Monit95App.Web/systemjs.config.js
+++ Monit95App.Web/systemjs.config.js
@@ -1,6 +1,5 @@
-﻿(function () {
-    //System.appVersion = '3';
-    var appVersion = '5';
+﻿(function () {    
+    var appVersionExtension = 'js?v=1';
     System.config({
         paths: {
             // псевдоним для пути к модулям
@@ -12,48 +11,48 @@
             app: 'app',
 
             // angular packages
-            '@angular/core': `npm:@angular/core/bundles/core.umd.js?v=${appVersion}`,
-            '@angular/common': `npm:@angular/common/bundles/common.umd.js?v=${appVersion}`,
-            '@angular/common/http': `npm:@angular/common/bundles/common-http.umd.js?v=${appVersion}`,
-            '@angular/compiler': `npm:@angular/compiler/bundles/compiler.umd.js?v=${appVersion}`,
-            '@angular/animations': `npm:@angular/animations/bundles/animations.umd.js?v=${appVersion}`,
-            '@angular/platform-browser': `npm:@angular/platform-browser/bundles/platform-browser.umd.js?v=${appVersion}`,
-            '@angular/animations/browser': `npm:@angular/animations/bundles/animations-browser.umd.js?v=${appVersion}`,
-            '@angular/platform-browser/animations': `npm:@angular/platform-browser/bundles/platform-browser-animations.umd.js?v=${appVersion}`,
-            '@angular/platform-browser-dynamic': `npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js?v=${appVersion}`,
-            '@angular/http': `npm:@angular/http/bundles/http.umd.js?v=${appVersion}`,
-            '@angular/router': `npm:@angular/router/bundles/router.umd.js?v=${appVersion}`,
-            '@angular/forms': `npm:@angular/forms/bundles/forms.umd.js?v=${appVersion}`,
-            '@angular/upgrade': `npm:@angular/upgrade/bundles/upgrade.umd.js?v=${appVersion}`,
-            '@angular/material': `npm:@angular/material/bundles/material.umd.js?v=${appVersion}`,
+            '@angular/core': `npm:@angular/core/bundles/core.umd.${appVersionExtension}`,
+            '@angular/common': `npm:@angular/common/bundles/common.umd.${appVersionExtension}`,
+            '@angular/common/http': `npm:@angular/common/bundles/common-http.umd.${appVersionExtension}`,
+            '@angular/compiler': `npm:@angular/compiler/bundles/compiler.umd.${appVersionExtension}`,
+            '@angular/animations': `npm:@angular/animations/bundles/animations.umd.${appVersionExtension}`,
+            '@angular/platform-browser': `npm:@angular/platform-browser/bundles/platform-browser.umd.${appVersionExtension}`,
+            '@angular/animations/browser': `npm:@angular/animations/bundles/animations-browser.umd.${appVersionExtension}`,
+            '@angular/platform-browser/animations': `npm:@angular/platform-browser/bundles/platform-browser-animations.umd.${appVersionExtension}`,
+            '@angular/platform-browser-dynamic': `npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.${appVersionExtension}`,
+            '@angular/http': `npm:@angular/http/bundles/http.umd.${appVersionExtension}`,
+            '@angular/router': `npm:@angular/router/bundles/router.umd.${appVersionExtension}`,
+            '@angular/forms': `npm:@angular/forms/bundles/forms.umd.${appVersionExtension}`,
+            '@angular/upgrade': `npm:@angular/upgrade/bundles/upgrade.umd.${appVersionExtension}`,
+            '@angular/material': `npm:@angular/material/bundles/material.umd.${appVersionExtension}`,
 
             // cdk individual packages            
-            '@angular/cdk': `npm:@angular/cdk/bundles/cdk.umd.js?v=${appVersion}`,
-            '@angular/cdk/a11y': `npm:@angular/cdk/bundles/cdk-a11y.umd.js?v=${appVersion}`,
-            '@angular/cdk/bidi': `npm:@angular/cdk/bundles/cdk-bidi.umd.js?v=${appVersion}`,
-            '@angular/cdk/coercion': `npm:@angular/cdk/bundles/cdk-coercion.umd.js?v=${appVersion}`,
-            '@angular/cdk/collections': `npm:@angular/cdk/bundles/cdk-collections.umd.js?v=${appVersion}`,
-            '@angular/cdk/keycodes': `npm:@angular/cdk/bundles/cdk-keycodes.umd.js?v=${appVersion}`,
-            '@angular/cdk/observers': `npm:@angular/cdk/bundles/cdk-observers.umd.js?v=${appVersion}`,
-            '@angular/cdk/overlay': `npm:@angular/cdk/bundles/cdk-overlay.umd.js?v=${appVersion}`,
-            '@angular/cdk/platform': `npm:@angular/cdk/bundles/cdk-platform.umd.js?v=${appVersion}`,
-            '@angular/cdk/portal': `npm:@angular/cdk/bundles/cdk-portal.umd.js?v=${appVersion}`,
-            '@angular/cdk/rxjs': `npm:@angular/cdk/bundles/cdk-rxjs.umd.js?v=${appVersion}`,
-            '@angular/cdk/scrolling': `npm:@angular/cdk/bundles/cdk-scrolling.umd.js?v=${appVersion}`,
-            '@angular/cdk/table': `npm:@angular/cdk/bundles/cdk-table.umd.js?v=${appVersion}`,
-            '@angular/cdk/stepper': `npm:@angular/cdk/bundles/cdk-stepper.umd.js?v=${appVersion}`,
+            '@angular/cdk': `npm:@angular/cdk/bundles/cdk.umd.${appVersionExtension}`,
+            '@angular/cdk/a11y': `npm:@angular/cdk/bundles/cdk-a11y.umd.${appVersionExtension}`,
+            '@angular/cdk/bidi': `npm:@angular/cdk/bundles/cdk-bidi.umd.${appVersionExtension}`,
+            '@angular/cdk/coercion': `npm:@angular/cdk/bundles/cdk-coercion.umd.${appVersionExtension}`,
+            '@angular/cdk/collections': `npm:@angular/cdk/bundles/cdk-collections.umd.${appVersionExtension}`,
+            '@angular/cdk/keycodes': `npm:@angular/cdk/bundles/cdk-keycodes.umd.${appVersionExtension}`,
+            '@angular/cdk/observers': `npm:@angular/cdk/bundles/cdk-observers.umd.${appVersionExtension}`,
+            '@angular/cdk/overlay': `npm:@angular/cdk/bundles/cdk-overlay.umd.${appVersionExtension}`,
+            '@angular/cdk/platform': `npm:@angular/cdk/bundles/cdk-platform.umd.${appVersionExtension}`,
+            '@angular/cdk/portal': `npm:@angular/cdk/bundles/cdk-portal.umd.${appVersionExtension}`,
+            '@angular/cdk/rxjs': `npm:@angular/cdk/bundles/cdk-rxjs.umd.${appVersionExtension}`,
+            '@angular/cdk/scrolling': `npm:@angular/cdk/bundles/cdk-scrolling.umd.${appVersionExtension}`,
+            '@angular/cdk/table': `npm:@angular/cdk/bundles/cdk-table.umd.${appVersionExtension}`,
+            '@angular/cdk/stepper': `npm:@angular/cdk/bundles/cdk-stepper.umd.${appVersionExtension}`,
 
-            '@ng-bootstrap/ng-bootstrap': `npm:@ng-bootstrap/ng-bootstrap/bundles/ng-bootstrap.js?v=${appVersion}`,
-            'angular-in-memory-web-api': `npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js?v=${appVersion}`,
-            'tslib': `npm:tslib/tslib.js?v=${appVersion}`,
-            'ngx-order-pipe': `npm:ngx-order-pipe/dist/bundles/ngx-order-pipe.umd.js?v=${appVersion}`,
+            '@ng-bootstrap/ng-bootstrap': `npm:@ng-bootstrap/ng-bootstrap/bundles/ng-bootstrap.${appVersionExtension}`,
+            'angular-in-memory-web-api': `npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.${appVersionExtension}`,
+            'tslib': `npm:tslib/tslib.${appVersionExtension}`,
+            'ngx-order-pipe': `npm:ngx-order-pipe/dist/bundles/ngx-order-pipe.umd.${appVersionExtension}`,
 
             // пакеты angular2-modal     
-            'angular2-modal': `npm:angular2-modal/bundle/angular2-modal.rollup.umd.js?v=${appVersion}`,
-            'angular2-modal/plugins/bootstrap': `npm:angular2-modal/plugins/bootstrap/bundle/angular2-modal-bootstrap.rollup.umd.js?v=${appVersion}`,               
+            'angular2-modal': `npm:angular2-modal/bundle/angular2-modal.rollup.umd.${appVersionExtension}`,
+            'angular2-modal/plugins/bootstrap': `npm:angular2-modal/plugins/bootstrap/bundle/angular2-modal-bootstrap.rollup.umd.${appVersionExtension}`,               
 			
             // datepicker
-            'mydatepicker': `npm:mydatepicker/bundles/mydatepicker.umd.min.js?v=${appVersion}`,
+            'mydatepicker': `npm:mydatepicker/bundles/mydatepicker.umd.min.${appVersionExtension}`,
             
             'rxjs': 'npm:rxjs',           
         },
@@ -61,11 +60,11 @@
         packages: {
             app: {
                 main: './main',
-                defaultExtension: `js?v=${appVersion}`
+                defaultExtension: appVersionExtension
             },
            
             rxjs: {
-                defaultExtension: 'js'
+                defaultExtension: appVersionExtension
             }
         }
     });
