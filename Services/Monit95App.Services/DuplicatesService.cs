570f0c10be6efb3a61b436d1f599564582131c1e
Хусайн Эсамбаев <husainesambaev@gmail.com>
2017-11-16 18:01:06 +0300

    comm

---
+++ Services/Monit95App.Services/DuplicatesService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Monit95App.Services
+{
+    public class DuplicatesService
+    {
+        public IEnumerable<string> GetNonDuplicateFiles(string[] fileNames, string distFolder)
+        {
+            List<string> hashes = new List<string>();
+            byte[] hash;
+            string hashString;
+            foreach (var fileName in fileNames)
+            {
+                using (var md5 = MD5.Create())
+                {
+                    using (var stream = File.OpenRead(fileName))
+                    {
+                        hash = md5.ComputeHash(stream);
+                        hashString = BitConverter.ToString(hash).ToLowerInvariant();
+                    }
+                }
+
+                if (!hashes.Contains(hashString))
+                {
+                    hashes.Add(hashString);
+                    File.Move(fileName, $"{distFolder}\\{Path.GetFileName(fileName)}");
+                }
+            }
+
+            return Directory.GetFiles(distFolder);
+        }
+    }
+}
---
+++ TESTS/Monit95App.Infrastructure.Business.Tests/DuplicatesService_Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
+
+namespace Monit95App.Services.Tests
+{
+    [TestClass]
+    public class DuplicatesService_Tests
+    {
+        [TestMethod]
+        public void DeleteDuplicates()
+        {
+            var service = new DuplicatesService();
+            var actual = service.GetNonDuplicateFiles(Directory.GetFiles("C:\\repositories\\tests"), "C:\\repositories\\tests_dist");
+
+            var expected = 2;
+
+            Assert.AreEqual(expected, actual.Count());
+        }
+    }
+}
