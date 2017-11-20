e72643f991451dc4eb2837cafb99663e9882b8ce
Adam Shakhabov <theadamo86@outlook.de>
2017-11-20 11:42:24 +0300

    Change

--- SERVICES/Monit95App.Services/Rsur/RsurMarksService.cs
+++ SERVICES/Monit95App.Services/Rsur/RsurMarksService.cs
@@ -5,16 +5,11 @@ using Monit95App.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Monit95App.Services
 {
     public class RsurMarksService : IRsurMarksService
-    {
-        //private readonly string[] DATY_Mark_Names = new string[] { "2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7", "2.8", "2.9", "2.10", "5.1", "5.2", "5.3", "5.4", "5.5", "5.6", "5.7", "5.8", "8.1", "8.2", "8.3", "8.4", "8.5", "8.6", "8.7" };
-        //private readonly string[] TERMINY_Mark_Names = new string[] { "10.1", "10.2", "10.3", "10.4", "10.5", "10.6", "10.7", "10.8", "10.9", "10.10", "9.1", "9.2", "9.3", "9.4", "9.5", "9.6", "9.7", "9.8", "9.9", "8.1", "8.2", "8.3", "8.4", "8.5", "8.6", "8.7", "8.8" };
-
+    {        
         private readonly string[] Block_0101 = new string[] { "8.1", "8.2", "8.3",
                                                               "9.1", "9.2", "9.3",
                                                               "10.1", "10.2", "10.3",
@@ -25,11 +20,11 @@ namespace Monit95App.Services
 
         private readonly IGenericRepository<RsurParticipTest> _participTestRepository;
         private readonly IGenericRepository<RsurParticip> _participRepository;
-        private readonly IGenericRepository<Domain.Core.Entities.RsurTestResult> _resultRepository;
+        private readonly IGenericRepository<RsurTestResult> _resultRepository;
 
         public RsurMarksService(IGenericRepository<RsurParticipTest> participTestRepository, 
                                 IGenericRepository<RsurParticip> participRepository,
-                                IGenericRepository<Domain.Core.Entities.RsurTestResult> resultRepository)
+                                IGenericRepository<RsurTestResult> resultRepository)
         {
             _participTestRepository = participTestRepository;
             _participRepository = participRepository;
@@ -66,7 +61,7 @@ namespace Monit95App.Services
             var rsurResult = _resultRepository.GetById(participTestId);
             if(rsurResult == null)
             {
-                var rsurTestResult = new Domain.Core.Entities.RsurTestResult
+                var rsurTestResult = new RsurTestResult
                 {
                     RsurParticipTestId = participTestId,
                     RsurQuestionValues = marks
