034ce61266df2cfe1163706ce9eae50b28460aab
Adam Shakhabov <theadamo86@outlook.de>
2017-11-24 14:11:02 +0300

    Change

--- TESTS/Monit95App.Services.Tests/MarksProtocolServiceTest.cs
+++ TESTS/Monit95App.Services.Tests/MarksProtocolServiceTest.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json.Linq;
-using System.IO;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Data.Entity;
+using Monit95App.Infrastructure.Data;
+using Newtonsoft.Json;
+using Monit95App.Services.Rsur.MarksProtocol;
 using System.Collections.Generic;
 using Monit95App.Domain.Core.Entities;
-using System.Linq;
 
 namespace Monit95App.Services.Tests
 {
@@ -13,21 +14,55 @@ namespace Monit95App.Services.Tests
     {
         [TestMethod]
         public void GetTest()
-        {                        
-            // Arrange
-            var jArray = JArray.Parse(System.IO.File.ReadAllText($@"{Directory.GetCurrentDirectory()}\json-mock\RsurParticipTestResults.json"));
-            var l = jArray.ToObject<List<RsurTestResult>>();
-
-            foreach(var item in l)
+        {
+            // Arrange         
+            var fakeRsurTestResults = new List<RsurTestResult>
             {
-                Console.WriteLine(item.Grade5);
-            }
+                new RsurTestResult
+                {
+                    RsurQuestionValues = "0;1",
+                    RsurParticipTestId = 1,
+                    RsurParticipTest = new RsurParticipTest
+                    {
+                        RsurParticipCode = 12345,
+                        RsurTest = new RsurTest
+                        {
+                            IsOpen = true,
+                            Test = new Test
+                            {
+                                NumberCode = "0101",
+                                Name = "Орфография",
+                                TestQuestions = new List<TestQuestion>
+                                {
+                                    new TestQuestion
+                                    {
+                                        Order = 1,
+                                        Name = "1",
+                                        Question = new Question
+                                        {
+                                            MaxMark = 1
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        RsurParticip = new RsurParticip
+                        {
+                            School = new Domain.Core.Entities.School
+                            {
+                                AreaCode = 201
+                            }
+                        }
+                    }
+                }
+            }.AsQueryable();
 
-            // Act
-            var result = l.Single(x => x.RsurParticipTest.RsurParticip.Code == 10928);
+            var service = new MarksProtocolService(new CokoContext());
 
-            // Assert
-            
+            // Act            
+            var result = service.Get(12345, 201);
+
+            // Assert            
         }
     }
 }
