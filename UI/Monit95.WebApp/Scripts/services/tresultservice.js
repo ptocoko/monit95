afda5594e2ce8c0735245a7ecf0e90f420896b33
Adam Shakhabov <theadamo86@outlook.de>
2017-12-04 22:17:48 +0300

    Refactoring

--- UI/Monit95.WebApp/Scripts/services/tresultservice.js
+++
@@ -1,7 +0,0 @@
-﻿nsurApp.factory('TResultService', function ($http) {
-    var fac = {};
-    fac.GetOpenProjectTestDTOs = function (projectCode, areaCode) {
-        return $http.get('/api/ExerMark/GetOpenProjectTestDTOs?projectCode=' + projectCode + '&areaCode=' + areaCode);
-    }
-    return fac;
-});
