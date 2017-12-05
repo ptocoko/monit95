a9cb36fbdbf3dd46488b4362311364edeb909b08
Adam Shakhabov <theadamo86@outlook.de>
2017-12-05 11:40:47 +0300

    Add

--- DOMAIN/Monit95.Domain.Core/Entities/File.cs
+++ DOMAIN/Monit95.Domain.Core/Entities/File.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Monit95App.Domain.Core.Entities
 {
@@ -20,6 +16,8 @@ namespace Monit95App.Domain.Core.Entities
         [Required]
         [StringLength(100)]
         public string Name { get; set; }
+
+        public byte[] Hash { get; set; }
 
         public int RepositoryId { get; set; }
 
