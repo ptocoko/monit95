05617187555ba833d188529676bf7896203fef93
Хусайн Эсамбаев <husainesambaev@gmail.com>
2017-12-05 09:40:09 +0300

    publish settings

--- Monit95App.Web/systemjs.config.js
+++ Monit95App.Web/systemjs.config.js
@@ -1,5 +1,5 @@
 ﻿(function () {    
-    const appVersionExtension = 'js?v=5';
+    const appVersionExtension = 'js?v=7';
     System.config({
         paths: {
             // псевдоним для пути к модулям
