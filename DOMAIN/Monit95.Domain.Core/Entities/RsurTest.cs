81d48705ed1b14b0b02f96e385666dc6d5233ee0
Adam Shakhabov <theadamo86@outlook.de>
2017-12-25 09:33:40 +0300

    Change

--- DOMAIN/Monit95.Domain.Core/Entities/RsurTest.cs
+++ DOMAIN/Monit95.Domain.Core/Entities/RsurTest.cs
@@ -4,7 +4,6 @@ namespace Monit95App.Domain.Core.Entities
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     public partial class RsurTest
     {
