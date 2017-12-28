277fd4f4611ab44d99a3d652102711324c70c0d4
Хусайн Эсамбаев <husainesambaev@gmail.com>
2017-12-28 12:04:20 +0300

    finish table materializing

--- UI/Monit95.WebApp/app/components/rsur/reports/report-list/report-list.component.js
+++ UI/Monit95.WebApp/app/components/rsur/reports/report-list/report-list.component.js
@@ -5,12 +5,15 @@ var core_1 = require("@angular/core");
 var router_1 = require("@angular/router");
 var rsur_report_service_1 = require("../../../../services/rsur-report.service");
 var account_service_1 = require("../../../../services/account.service");
+var material_1 = require("@angular/material");
 var TEST_DATE = '2017-10-11';
 var ReportListComponent = /** @class */ (function () {
     function ReportListComponent(rsurReportService, route, accountService) {
         this.rsurReportService = rsurReportService;
         this.route = route;
         this.accountService = accountService;
+        this.displayedColumns = ['number', 'code', 'surname', 'name', 'secondName', 'schoolName', 'examName', 'testStatus'];
+        this.dataSource = new material_1.MatTableDataSource();
     }
     ReportListComponent.prototype.ngOnInit = function () {
         var _this = this;
@@ -23,8 +26,41 @@ var ReportListComponent = /** @class */ (function () {
         this.isLoading = true;
         this.rsurReportService.getReports().subscribe(function (reports) {
             _this.reportsList = reports;
+            _this.dataSource = new material_1.MatTableDataSource(reports);
+            $().ready(function () { return _this.dataSource.paginator = _this.paginator; });
             _this.isLoading = false;
         });
+    };
+    ReportListComponent.prototype.searchParticip = function (searchText) {
+        this.dataSource.filterPredicate = participSearchPredicate;
+        this.dataSource.filter = searchText.toLowerCase();
+    };
+    ReportListComponent.prototype.filterByExamName = function (examName) {
+        this.dataSource.filterPredicate = examNameFilterPredicate;
+        if (examName === 'все диагностики') {
+            this.dataSource.filter = null;
+        }
+        else {
+            this.dataSource.filter = examName;
+        }
+    };
+    ReportListComponent.prototype.filterByTestName = function (testName) {
+        this.dataSource.filterPredicate = testNameFilterPredicate;
+        if (testName === 'все блоки') {
+            this.dataSource.filter = null;
+        }
+        else {
+            this.dataSource.filter = testName;
+        }
+    };
+    ReportListComponent.prototype.filterBySchoolName = function (schoolName) {
+        this.dataSource.filterPredicate = schoolNameFilterPredicate;
+        if (schoolName === 'все организации') {
+            this.dataSource.filter = null;
+        }
+        else {
+            this.dataSource.filter = schoolName;
+        }
     };
     ReportListComponent.prototype.openReport = function (report) {
         if (report.TestStatus.toLowerCase() !== 'отсутствовал' && report.ExamName.toLowerCase() !== 'апрель-2017') {
@@ -39,6 +75,10 @@ var ReportListComponent = /** @class */ (function () {
         this.selectedTest = 'все блоки';
         this.selectedExam = 'все диагностики';
     };
+    tslib_1.__decorate([
+        core_1.ViewChild('paginator'),
+        tslib_1.__metadata("design:type", material_1.MatPaginator)
+    ], ReportListComponent.prototype, "paginator", void 0);
     ReportListComponent = tslib_1.__decorate([
         core_1.Component({
             selector: 'report-list',
@@ -52,4 +92,18 @@ var ReportListComponent = /** @class */ (function () {
     return ReportListComponent;
 }());
 exports.ReportListComponent = ReportListComponent;
+var participSearchPredicate = function (particip, searchText) {
+    return particip.ParticipCode.toString().indexOf(searchText) > -1 ||
+        particip.SchoolParticipInfo.Surname.toLowerCase().indexOf(searchText) > -1 ||
+        particip.SchoolParticipInfo.Name.toLowerCase().indexOf(searchText) > -1;
+};
+var schoolNameFilterPredicate = function (report, schoolName) {
+    return report.SchoolParticipInfo.SchoolName === schoolName;
+};
+var testNameFilterPredicate = function (report, testName) {
+    return report.TestName === testName;
+};
+var examNameFilterPredicate = function (report, examName) {
+    return report.ExamName === examName;
+};
 //# sourceMappingURL=report-list.component.js.map
--- UI/Monit95.WebApp/app/components/rsur/reports/report-list/report-list.component.ts
+++ UI/Monit95.WebApp/app/components/rsur/reports/report-list/report-list.component.ts
@@ -1,8 +1,9 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { RsurReportService } from '../../../../services/rsur-report.service';
 import { AccountService } from '../../../../services/account.service';
 import { RsurReportModel } from '../../../../models/rsur-report.model';
+import { MatTableDataSource, MatSelectChange, MatPaginator } from '@angular/material';
 
 const TEST_DATE = '2017-10-11';
 
@@ -13,14 +14,16 @@ const TEST_DATE = '2017-10-11';
 })
 export class ReportListComponent implements OnInit {
     reportsList: RsurReportModel[];
-    
 	isLoading: boolean;
+	displayedColumns = ['number', 'code', 'surname', 'name', 'secondName', 'schoolName', 'examName', 'testStatus'];
+	dataSource = new MatTableDataSource();
 
     selectedSchool: string;
 	selectedTest: string;
 	selectedExam: string;
+    //searchParticip: string;
 
-    searchParticip: string;
+	@ViewChild('paginator') paginator: MatPaginator;
     
 	constructor(private readonly rsurReportService: RsurReportService, 
                 private readonly route: Router,
@@ -38,9 +41,47 @@ export class ReportListComponent implements OnInit {
 		this.isLoading = true;
         this.rsurReportService.getReports().subscribe(reports => {
 			this.reportsList = reports;           
-            
+			this.dataSource = new MatTableDataSource(reports);
+			$().ready(() => this.dataSource.paginator = this.paginator);
 	        this.isLoading = false;
 	    });
+	}
+
+	searchParticip(searchText: string) {
+		this.dataSource.filterPredicate = participSearchPredicate;
+
+		this.dataSource.filter = searchText.toLowerCase();
+	}
+
+	filterByExamName(examName: string) {
+		this.dataSource.filterPredicate = examNameFilterPredicate;
+
+		if (examName === 'все диагностики') {
+			this.dataSource.filter = null;
+		}
+		else {
+			this.dataSource.filter = examName;
+		}
+	}
+
+	filterByTestName(testName: string) {
+		this.dataSource.filterPredicate = testNameFilterPredicate;
+		if (testName === 'все блоки') {
+			this.dataSource.filter = null;
+		}
+		else {
+			this.dataSource.filter = testName;
+		}
+	}
+
+	filterBySchoolName(schoolName: string) {
+		this.dataSource.filterPredicate = schoolNameFilterPredicate;
+		if (schoolName === 'все организации') {
+			this.dataSource.filter = null;
+		}
+		else {
+			this.dataSource.filter = schoolName;
+		}
 	}
 
 	openReport(report: RsurReportModel) {
@@ -57,4 +98,22 @@ export class ReportListComponent implements OnInit {
 		this.selectedTest = 'все блоки';
 		this.selectedExam = 'все диагностики';
 	}
+}
+
+const participSearchPredicate = (particip: RsurReportModel, searchText: string) => {
+	return particip.ParticipCode.toString().indexOf(searchText) > -1 ||
+		particip.SchoolParticipInfo.Surname.toLowerCase().indexOf(searchText) > -1 ||
+		particip.SchoolParticipInfo.Name.toLowerCase().indexOf(searchText) > -1;
+}
+
+const schoolNameFilterPredicate = (report: RsurReportModel, schoolName: string) => {
+	return report.SchoolParticipInfo.SchoolName === schoolName;
+}
+
+const testNameFilterPredicate = (report: RsurReportModel, testName: string) => {
+	return report.TestName === testName;
+}
+
+const examNameFilterPredicate = (report: RsurReportModel, examName: string) => {
+	return report.ExamName === examName;
 }
