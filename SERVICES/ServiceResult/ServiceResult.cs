b9094d69c91d8a6a926c3c077d2120b7a48d7406
Adam Shakhabov <theadamo86@outlook.de>
2018-01-07 03:22:10 +0300

    Change

--- SERVICES/ServiceResult/ServiceResult.cs
+++ SERVICES/ServiceResult/ServiceResult.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ServiceResult
 {
@@ -8,7 +10,29 @@ namespace ServiceResult
         /// Gets or sets result which the method return
         /// </summary>
         public T Result { get; set; }
+        
+        public List<ServiceError> Errors { get; set; } = new List<ServiceError>();
 
-        public List<ServiceError> Errors { get; set; } = new List<ServiceError>();           
+        /// <summary>
+        /// Добавление информации об ошибке
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="description"></param>
+        /// <param name="httpStatusCode">Соответствующий статусный код http</param>
+        /// TODO: подумать над тем чтобы создать вот такое "if (StringValidate.AnyNullOrWhiteSpace(key, description, out errorMessage)), т.е. errorMessage уже содержал бы "{nameof(key)} or {nameof(description)} is null or empty"        
+        public void AddModelError(string key, string description, int? httpStatusCode)
+        {
+            if (StringValidate.AnyNullOrWhiteSpace(key, description))
+                throw new ArgumentException($"{nameof(key)} or {nameof(description)} is null or empty");
+
+            var error = new ServiceError
+            {
+                Key = key,
+                Description = description
+            };
+            if (httpStatusCode != null && Enumerable.Range(100, 526).Contains((int) httpStatusCode))
+                error.HttpCode = httpStatusCode;  
+            Errors.Add(error);
+        }
     }
 }
