2b40a4c49ef8a5f7486980441a44a3149c480508
Хусайн Эсамбаев <husainesambaev@gmail.com>
2018-01-29 12:22:54 +0300

    serverlessed http-services' code for testing without backend

--- UI/Monit95.WebApp/app/services/particip.service.js
+++ UI/Monit95.WebApp/app/services/particip.service.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var tslib_1 = require("tslib");
 var core_1 = require("@angular/core");
 var http_1 = require("@angular/common/http");
+var Observable_1 = require("rxjs/Observable");
 var ParticipService = /** @class */ (function () {
     function ParticipService(http) {
         this.http = http;
@@ -11,19 +12,26 @@ var ParticipService = /** @class */ (function () {
         this.endpoint = "/api/particips/";
     }
     ParticipService.prototype.getAll = function (projectId) {
-        return this.http.get(this.GET_ALL_PARTICIPS_URL + projectId);
+        //return this.http.get<ParticipModel[]>(this.GET_ALL_PARTICIPS_URL + projectId);
+        return Observable_1.Observable.of(mockParticipsList);
     };
     ParticipService.prototype.getParticip = function (participId) {
-        return this.http.get(this.endpoint + participId);
+        //return this.http.get<ParticipModel>(this.endpoint + participId);
+        return Observable_1.Observable.of(mockParticipsList.find(function (f) { return f.Id === participId; }));
     };
     ParticipService.prototype.addParticip = function (particip) {
-        return this.http.post(this.endpoint, particip, { responseType: 'text' });
+        //return this.http.post(this.endpoint, particip, { responseType: 'text' });
+        mockParticipsList.push(particip);
+        return Observable_1.Observable.of('hehe');
     };
     ParticipService.prototype.updateParticip = function (particip) {
         throw Error('this method not implemented');
     };
-    ParticipService.prototype.deleteParticip = function (participId) {
-        return this.http.delete(this.endpoint + participId, { responseType: 'text' });
+    ParticipService.prototype.deleteParticip = function (documNumber) {
+        //return this.http.delete(this.endpoint + participId, { responseType: 'text' });
+        var participIndex = mockParticipsList.indexOf(mockParticipsList.find(function (f) { return f.DocumNumber === documNumber; }));
+        mockParticipsList.splice(participIndex, 1);
+        return Observable_1.Observable.of('hah');
     };
     ParticipService = tslib_1.__decorate([
         core_1.Injectable(),
@@ -32,4 +40,45 @@ var ParticipService = /** @class */ (function () {
     return ParticipService;
 }());
 exports.ParticipService = ParticipService;
+var mockParticipsList = [
+    {
+        Id: 1,
+        Surname: 'Test1',
+        Name: 'Test2',
+        SecondName: 'Test3',
+        ProjectId: 12,
+        DocumNumber: 12345,
+        SourceName: 'РЦОИ',
+        ClassName: '1a',
+        Birthday: '12.11.2009',
+        SchoolId: '0005',
+        SchoolName: 'президентская школа'
+    },
+    {
+        Id: 2,
+        Surname: 'Test1',
+        Name: 'Test2',
+        SecondName: 'Test3',
+        ProjectId: 12,
+        DocumNumber: 65432,
+        SourceName: 'Школа',
+        ClassName: '1a',
+        Birthday: '12.11.2009',
+        SchoolId: '0005',
+        SchoolName: 'президентская школа'
+    },
+    {
+        Id: 3,
+        Surname: 'длиннаяфамилия',
+        Name: 'heheheheh',
+        SecondName: 'оченьдлинноеотчество',
+        ProjectId: 12,
+        DocumNumber: 98745,
+        SourceName: 'Школа',
+        ClassName: '1a',
+        Birthday: '12.11.2009',
+        SchoolId: '0005',
+        SchoolName: 'президентская школа'
+    }
+];
 //# sourceMappingURL=particip.service.js.map
--- UI/Monit95.WebApp/app/services/particip.service.ts
+++ UI/Monit95.WebApp/app/services/particip.service.ts
@@ -12,23 +12,72 @@ export class ParticipService {
 	
 	constructor(private http: HttpClient) { }
 
-	getAll(projectId: number) {
-		return this.http.get<ParticipModel[]>(this.GET_ALL_PARTICIPS_URL + projectId);
+	getAll(projectId: number): Observable<ParticipModel[]> {
+		//return this.http.get<ParticipModel[]>(this.GET_ALL_PARTICIPS_URL + projectId);
+		return Observable.of(mockParticipsList);
 	}
 
 	getParticip(participId: number) {
-		return this.http.get<ParticipModel>(this.endpoint + participId);
+		//return this.http.get<ParticipModel>(this.endpoint + participId);
+		return Observable.of(mockParticipsList.find(f => f.Id === participId));
 	}
 
 	addParticip(particip: ParticipModel) {
-		return this.http.post(this.endpoint, particip, { responseType: 'text' });
+		//return this.http.post(this.endpoint, particip, { responseType: 'text' });
+		mockParticipsList.push(particip);
+		return Observable.of('hehe');
 	}
 
 	updateParticip(particip: ParticipModel) {
 		throw Error('this method not implemented');
 	}
 
-	deleteParticip(participId: number) {
-		return this.http.delete(this.endpoint + participId, { responseType: 'text' });
+	deleteParticip(documNumber: number) {
+		//return this.http.delete(this.endpoint + participId, { responseType: 'text' });
+		const participIndex = mockParticipsList.indexOf(mockParticipsList.find(f => f.DocumNumber === documNumber));
+		mockParticipsList.splice(participIndex, 1);
+		return Observable.of('hah');
 	}
 }
+
+const mockParticipsList: ParticipModel[] = [
+	{
+		Id: 1,
+		Surname: 'Test1',
+		Name: 'Test2',
+		SecondName: 'Test3',
+		ProjectId: 12,
+		DocumNumber: 12345,
+		SourceName: 'РЦОИ',
+		ClassName: '1a',
+		Birthday: '12.11.2009',
+		SchoolId: '0005',
+		SchoolName: 'президентская школа'
+	},
+	{
+		Id: 2,
+		Surname: 'Test1',
+		Name: 'Test2',
+		SecondName: 'Test3',
+		ProjectId: 12,
+		DocumNumber: 65432,
+		SourceName: 'Школа',
+		ClassName: '1a',
+		Birthday: '12.11.2009',
+		SchoolId: '0005',
+		SchoolName: 'президентская школа'
+	},
+	{
+		Id: 3,
+		Surname: 'длиннаяфамилия',
+		Name: 'heheheheh',
+		SecondName: 'оченьдлинноеотчество',
+		ProjectId: 12,
+		DocumNumber: 98745,
+		SourceName: 'Школа',
+		ClassName: '1a',
+		Birthday: '12.11.2009',
+		SchoolId: '0005',
+		SchoolName: 'президентская школа'
+	}
+]
