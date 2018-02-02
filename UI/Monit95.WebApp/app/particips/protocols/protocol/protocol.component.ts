5db96fc5c9c19f82b17ed4e2fb102cf77934b51c
Хусайн Эсамбаев <husainesambaev@gmail.com>
2018-02-02 09:00:39 +0300

    some fixes

--- UI/Monit95.WebApp/app/particips/protocols/protocol/protocol.component.ts
+++ UI/Monit95.WebApp/app/particips/protocols/protocol/protocol.component.ts
@@ -5,6 +5,7 @@ import { ParticipProtocolsService } from '../../../services/particip-protocols.s
 import { ParticipProtocolModel } from '../../../models/particip-protocol.model';
 import { QuestionResult } from '../../../models/marks-protocol.model';
 import { QuestionProtocolEdit } from '../../../models/question-protocol-edit.model';
+import { QuestionProtocolPost } from '../../../models/question-protocol-post.model';
 
 
 @Component({
@@ -15,6 +16,8 @@ export class ParticipProtocolComponent implements OnInit {
     isUpdate: boolean;
 	participTestId: number;   
 	protocol: QuestionProtocolEdit;
+	questionResults: QuestionResult[];
+	restMethod: 'POST' | 'PUT';
 
     constructor(
 		private readonly location: Location,
@@ -24,16 +27,36 @@ export class ParticipProtocolComponent implements OnInit {
     ngOnInit() {
         this.activatedRoute.params.subscribe(params => {
 			this.participTestId = Number.parseInt(params['id']);
+			this.restMethod = 'POST';
+
 			this.protocolsService.getProtocol(this.participTestId).subscribe(res => {
 				this.protocol = res;
+				this.questionResults = res.MarkCollection.map(val => {
+					let questionRes: QuestionResult = {
+						Order: val.Order,
+						CurrentMark: val.AwardedMark,
+						Name: val.Order.toString(),
+						MaxMark: val.MaxMark
+					};
+					return questionRes;
+				});
 			});
         });
     }
 
-    submit(questionResults: QuestionResult[]) {
-		this.protocolsService
-				.postMarksProtocol(questionResults, this.participTestId)
+	submit(questionResults: QuestionResult[]) {
+		if (this.restMethod === 'POST') {
+			let questionResultsPost = questionResults.map(val => {
+				let result: QuestionProtocolPost = {
+					AwardedMark: val.CurrentMark,
+					Order: val.Order
+				};
+				return result;
+			});
+			this.protocolsService
+				.postMarksProtocol(questionResultsPost, this.participTestId)
 				.subscribe(_ => this.back());
+		}
     }
 
     back() {
