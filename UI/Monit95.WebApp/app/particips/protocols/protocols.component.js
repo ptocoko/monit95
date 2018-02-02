57983f2933a59aa9592883500af368c6d5ea99f3
Хусайн Эсамбаев <husainesambaev@gmail.com>
2018-02-02 18:02:57 +0300

    materializing protocols list

--- UI/Monit95.WebApp/app/particips/protocols/protocols.component.js
+++ UI/Monit95.WebApp/app/particips/protocols/protocols.component.js
@@ -4,32 +4,55 @@ var tslib_1 = require("tslib");
 var core_1 = require("@angular/core");
 var router_1 = require("@angular/router");
 var particip_protocols_service_1 = require("../../services/particip-protocols.service");
+var material_1 = require("@angular/material");
 var PROJECT_TEST_ID = 1;
 var ProtocolsComponent = /** @class */ (function () {
     function ProtocolsComponent(participProtocolsService, router) {
         var _this = this;
         this.participProtocolsService = participProtocolsService;
         this.router = router;
+        this.displayedColumns = ['index', 'FIO', 'Marks', 'actions'];
         this.AbsentText = 'отсутствовал';
+        this.dataSource = new material_1.MatTableDataSource();
+        this.isLoading = true;
         // вычисление статистики
         this.processedProtocols = function () { return _this.protocols.filter(function (f) { return f.QuestionMarks; }).length; };
         this.notProcessedProtocols = function () { return _this.protocols.filter(function (f) { return !f.QuestionMarks; }).length; };
     }
     ProtocolsComponent.prototype.ngOnInit = function () {
+        this.getProtocols();
+    };
+    ProtocolsComponent.prototype.getProtocols = function () {
         var _this = this;
+        this.isLoading = true;
         this.participProtocolsService.getProtocolsList().subscribe(function (res) {
             _this.protocols = res;
+            _this.protocolsCount = res.length;
+            _this.dataSource = new material_1.MatTableDataSource(res);
+            _this.isLoading = false;
+            _this.dataSource.paginator = _this.paginator;
         });
     };
     ProtocolsComponent.prototype.changeMarks = function (participTestId) {
         this.router.navigate(['/particips/protocol', participTestId]);
     };
+    ProtocolsComponent.prototype.applyFilter = function (searchText) {
+        // во время поиска сбрасываем paginator на первую страницу
+        this.paginator.pageIndex = 0;
+        searchText = searchText.trim().toLowerCase();
+        this.dataSource.filter = searchText;
+    };
     ProtocolsComponent.prototype.markAsAbsent = function (protocol) {
         var _this = this;
         this.participProtocolsService.markAsAbsent(protocol.ParticipTestId).subscribe(function (_) {
-            protocol.QuestionMarks = _this.AbsentText;
+            //protocol.QuestionMarks = this.AbsentText;
+            _this.getProtocols();
         });
     };
+    tslib_1.__decorate([
+        core_1.ViewChild(material_1.MatPaginator),
+        tslib_1.__metadata("design:type", material_1.MatPaginator)
+    ], ProtocolsComponent.prototype, "paginator", void 0);
     ProtocolsComponent = tslib_1.__decorate([
         core_1.Component({
             templateUrl: "./app/particips/protocols/protocols.component.html?v=" + new Date().getTime(),
--- UI/Monit95.WebApp/app/particips/protocols/protocols.component.ts
+++ UI/Monit95.WebApp/app/particips/protocols/protocols.component.ts
@@ -1,8 +1,9 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, ViewChild } from '@angular/core';
 import { Router } from "@angular/router";
 import { ParticipProtocolsService } from '../../services/particip-protocols.service';
 import { Observable } from 'rxjs/Observable';
 import { QuestionProtocolRead } from '../../models/question-protocol-read.model';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
 
 const PROJECT_TEST_ID: number = 1;
 
@@ -11,8 +12,14 @@ const PROJECT_TEST_ID: number = 1;
 	styleUrls: [`./app/particips/protocols/protocols.component.css?v=${new Date().getTime()}`]
 })
 export class ProtocolsComponent {
+	displayedColumns = ['index', 'FIO', 'Marks', 'actions'];
+	protocolsCount: number;
 	AbsentText = 'отсутствовал';
+	dataSource = new MatTableDataSource<QuestionProtocolRead>();
 	protocols: QuestionProtocolRead[];
+	isLoading = true;
+
+	@ViewChild(MatPaginator) paginator: MatPaginator;
 
 	// вычисление статистики
 	processedProtocols = () => this.protocols.filter(f => f.QuestionMarks).length;
@@ -22,19 +29,37 @@ export class ProtocolsComponent {
 				private router: Router) { }
 
 	ngOnInit() {
-	    this.participProtocolsService.getProtocolsList().subscribe(res => {
-			this.protocols = res;
-	    });
+		this.getProtocols();
 	}
 	
+	getProtocols() {
+		this.isLoading = true;
+	    this.participProtocolsService.getProtocolsList().subscribe(res => {
+			this.protocols = res;
+			this.protocolsCount = res.length;
+			this.dataSource = new MatTableDataSource<QuestionProtocolRead>(res);
+			this.isLoading = false;
+			this.dataSource.paginator = this.paginator;
+	    });
+
+	}
 
 	changeMarks(participTestId: number) {
 		this.router.navigate(['/particips/protocol', participTestId]);
 	}
 
+	applyFilter(searchText: string) {
+		// во время поиска сбрасываем paginator на первую страницу
+		this.paginator.pageIndex = 0;
+
+		searchText = searchText.trim().toLowerCase();
+		this.dataSource.filter = searchText;
+	}
+
 	markAsAbsent(protocol: QuestionProtocolRead) {
 		this.participProtocolsService.markAsAbsent(protocol.ParticipTestId).subscribe(_ => {
-			protocol.QuestionMarks = this.AbsentText;
+			//protocol.QuestionMarks = this.AbsentText;
+			this.getProtocols();
 		});
 	}
 }
