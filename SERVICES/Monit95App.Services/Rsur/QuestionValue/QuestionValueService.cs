f27f2e7fbe5a78dba6058195021c29ca90b71f7e
Хусайн Эсамбаев <husainesambaev@gmail.com>
2018-02-22 19:12:04 +0300

    fixed

--- SERVICES/Monit95App.Services/Rsur/QuestionValue/QuestionValueService.cs
+++ SERVICES/Monit95App.Services/Rsur/QuestionValue/QuestionValueService.cs
@@ -275,10 +275,22 @@ namespace Monit95App.Services.Rsur.QuestionValue
                 RsurQuestionValues = s.RsurTestResult.RsurQuestionValues,
                 TestName = s.RsurTest.Test.NumberCode + "-" + s.RsurTest.Test.Name
             })
-            .OrderBy(ob => ob.RsurQuestionValues, (key1) => 
+            .ToList()
+            .OrderBy(ob => ob.RsurQuestionValues, Comparer<string>.Create((str1, str2) =>
             {
-
-            });
+                if(str1 == null && str2 != null)
+                {
+                    return -1;
+                }
+                else if(str1 != null && str2 == null)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            })).ThenBy(tb => tb.ParticipCode);
 
             return result;
         }
