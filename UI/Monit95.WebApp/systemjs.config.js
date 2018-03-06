025acfa0f8d34ff056b38444c98ea7533970866f
Husain Esambaev <husainesambaev@gmail.com>
2018-03-06 10:36:44 +0300

    changed app version

--- UI/Monit95.WebApp/systemjs.config.js
+++ UI/Monit95.WebApp/systemjs.config.js
@@ -1,5 +1,5 @@
 ﻿(function () {    
-    const appVersionExtension = 'js?v=8';
+    const appVersionExtension = 'js?v=9';
     System.config({
         paths: {
             // псевдоним для пути к модулям
