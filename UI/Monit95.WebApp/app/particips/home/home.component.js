e54a4bb238fc2e80ae714c7801a4fce31c4084c4
Хусайн Эсамбаев <husainesambaev@gmail.com>
2018-05-03 17:00:09 +0300

    fix bug

--- UI/Monit95.WebApp/app/particips/home/home.component.js
+++ UI/Monit95.WebApp/app/particips/home/home.component.js
@@ -4,8 +4,12 @@ var tslib_1 = require("tslib");
 var core_1 = require("@angular/core");
 var HomeComponent = /** @class */ (function () {
     function HomeComponent() {
+        this.timeIsCome = false;
     }
-    HomeComponent.prototype.ngOnInit = function () { };
+    HomeComponent.prototype.ngOnInit = function () {
+        var date = new Date();
+        this.timeIsCome = (date.getDate() === 4 && date.getHours() >= 8) || date.getDate() > 4;
+    };
     HomeComponent = tslib_1.__decorate([
         core_1.Component({
             templateUrl: "./app/particips/home/home.component.html?v=" + new Date().getTime()
