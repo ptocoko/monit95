5b3cf025961ff6b25a9523d81eddde408166674d
Хусайн Эсамбаев <husainesambaev@gmail.com>
2019-12-27 09:32:10 +0300

    commit

--- UI/Monit95.WebApp/systemjs.config.js
+++ UI/Monit95.WebApp/systemjs.config.js
@@ -1,5 +1,5 @@
 ﻿(function () {    
-    const appVersionExtension = 'js?v=413';
+    const appVersionExtension = 'js?v=415';
     System.config({
         paths: {
             // псевдоним для пути к модулям
