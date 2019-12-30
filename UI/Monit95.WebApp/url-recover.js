24710604b341c32965f9dd065cfbfd35605bb0ed
Хусайн Эсамбаев <husainesambaev@gmail.com>
2019-12-30 15:47:45 +0300

    commit

--- UI/Monit95.WebApp/url-recover.js
+++ UI/Monit95.WebApp/url-recover.js
@@ -13,15 +13,19 @@ function fromDir(startPath, filter) {
 		const stat = fs.lstatSync(filename);
 		if (stat.isDirectory()) {
 			fromDir(filename, filter);
-		} else if (filename.test(filter)) {
+		} else if (filter.test(filename)) {
 			console.log(filename);
-			const content = fs.readFileSync(filename, { flag: 'r+' });
-			const templateUrlLine = content.toString().test(/templateUrl/);
-			console.log(content);
-			console.log(templateUrlLine);
-			break;
+			let content = fs.readFileSync(filename).toString();
+			if (/templateUrl.+/.test(content)) {
+				const urlLine = content.match(/templateUrl.+/)[0];
+				const url = urlLine.match(/\..+(?=`)/)[0];
+				const newUrl = url.split('/').pop().split('?')[0];
+				content = content.replace(/templateUrl.+/, 'templateUrl: \'./' + newUrl + '\',');
+
+				fs.writeFileSync(filename, content);
+			}
 		}
 	}
 }
 
-fromDir('./');
+fromDir('./', /component.ts$/);
